3a3d80d56fe04d62110b32e394687724aa455583
Fernando Barroso <barroso.ouharriet@gmail.com>
2024-10-21 17:06:18 -0300

    correccion

--- src/components/FoxLabMission.vue
+++ src/components/FoxLabMission.vue
@@ -72,37 +72,6 @@
           description: "Especialista en desarrollo full stack con énfasis en arquitectura de sistemas y optimización de rendimiento."
         },
         ]
-
-        const PROJECTS = [
-        {
-        title: "Ferreteando - Ordenando tus venta en dos click",
-        description: `simplifica la gestión de ventas y presupuestos en ferreterías y pequeños comercios. Permite actualizar precios en tiempo real y generar presupuestos rápidamente. Su interfaz intuitiva y automatización optimizan el flujo de trabajo.`,
-        image: "/628_1x_shots_so.webp",
-        tags: [TAGS.HTML, TAGS.CSS, TAGS.VUETIFY, TAGS.VUE, TAGS.PYTHON],
-        },
-        {
-        title: "PoroIA - ML para el Éxito en el Mundo de League of Legends",
-        description: `PoroIA analiza partidas clasificatorias de League of Legends en nivel Diamante, centradas en los primeros 10 minutos. Con 10,000 partidas, ofrece información para mejorar rendimiento y estrategias. Es ideal para equipos que buscan optimización.`,
-        link: "https://ferjapolis.github.io/Coderhouser-DS-TP0002/Resumen.html",
-        github: "https://github.com/Ferjapolis/Coderhouser-DS-TP0002",
-        image: "/LoL.webp",
-        tags: [TAGS.PYTHON, TAGS.PLOTLY],
-        },
-        {
-        title: "Sistema de Vivero Automatizado con Raspberry Pi",
-        description: `El proyecto automatiza el riego y monitoreo de un vivero con sensores de humedad, temperatura, presión y pH, gestionado desde una web en Flask. Los datos se registran para análisis predictivos que optimizan el cuidado de las plantas.`,
-        image: "/342shots_so.webp",
-        tags: [TAGS.PYTHON, TAGS.PLOTLY, TAGS.RPIO],
-        },
-        {
-        title: "Generador de Metadata a partir de tablas exceles",
-        description: `El script original se encarga de leer un archivo de Excel de entrada, procesar los datos de los distintos sheets (o "tablas") del archivo y generar un archivo de salida en formato Parquet con información sobre las relaciones de datos entre las tablas.`,
-        image: "/342shots_so.webp",
-        tags: [TAGS.PYTHON ],
-        },
-        // Puedes agregar más missiones aquí...
-      ]
-  
       return {
         selectedMember,
         missions
